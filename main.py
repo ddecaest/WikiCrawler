06bb004e019bb0e707b59052a891bc9a9404c589
Noelle <87539814+HeadacheEnthusiast@users.noreply.github.com>
2021-07-20 22:13:51 +0300

    It works okay UwU

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
-if __name__ == '__main__':
-    print("test")
-    print("test2")
+from bs4 import BeautifulSoup
+import requests
 
-print('something')
+def the_nicifier(unformat_str):
+    # TODO
+    # Makes strings legible by wikipedia search motor
+    temp_var = ''
+    for letter in unformat_str:
+        if letter == ' ':
+            temp_var += '_'
+        else:
+            temp_var += str.lower(letter)
+    return temp_var
+
+def wikilink_maker(entry1):
+    book_title = the_nicifier(entry1)
+    wiki_html = requests.get(f'https://en.wikipedia.org/wiki/{book_title}').text
+    return wiki_html
+
+usr_entry = input('tester: ')
+
+soup = BeautifulSoup(wikilink_maker(usr_entry), 'lxml')
+table = soup.find('td', class_='infobox-data').text
+
+print(table)
